96cf3fe30e7f198cceb0bc675d656ecec5f25dfc
JungYean Hong <hhhong127@gmail.com>
2021-02-22 22:09:23 +0900

    C++

---
+++ C++/ch07/ex07_pointer.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Dog{
+public:
+    int age;
+    string name;
+
+    Dog(){
+        age = 1;
+        name = "바둑이";
+    }
+
+    ~Dog() {}
+
+    int getAge() {return age;}
+    void setAge(int a) {age = a;}
+};
+
+int main(){
+    Dog *pDog = new Dog;
+
+    cout << "Age of dog: " << pDog->getAge() << endl;
+
+    pDog->setAge(3);
+    cout << "Age of dog: " << pDog->getAge() << endl;
+
+    delete pDog;
+    return 0;
+}
